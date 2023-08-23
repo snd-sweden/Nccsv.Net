6759914beb1a6e6df74e7600e2c0141b7a36e1fe
idaboren <idaboren@gmail.com>
2023-08-23 13:39:33 +0200

    added structure! and better names!

---
+++ NccsvConverter/NccsvConverter.MainProject/NccsvParser/Helpers/NccsvVerifierMethods.cs
@@ -0,0 +1,21 @@
+﻿using UtfUnknown;
+
+namespace NccsvConverter.MainProject.NccsvParser.Helpers
+{
+    public class NccsvVerifierMethods
+    {
+        // Returns true if input file is in utf-8 encoding, false if not
+        public bool Utf8Checker(string filePath)
+        {
+            DetectionResult result = CharsetDetector.DetectFromFile(filePath);
+            DetectionDetail resultDetected = result.Detected;
+            string encodingName = resultDetected.EncodingName;
+
+            if (encodingName == "utf-8")
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
--- nccsv_converter/nccsv_converter_console/Program.cs
+++
@@ -1,7 +0,0 @@
-﻿// See https://aka.ms/new-console-template for more information
-
-using nccsv_verifier;
-
-var nccsv = new NccsvVerifierMethods();
-Console.WriteLine("Hello, World!");
-Console.WriteLine(nccsv.Utf8Checker("C:\\SND_repos\\nccsv_netcdf\\nccsv_converter\\nccsv_verifier_tests\\testcase-utf-8-true.csv"));
