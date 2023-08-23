467dd6f47454dc0862776f11bc643d526e489da4
idaboren <idaboren@gmail.com>
2023-08-23 13:04:23 +0200

    finished utf-8 checker (#5) with tests (#24)

--- nccsv_converter/nccsv_verifier/NccsvVerifierMethods.cs
+++ nccsv_converter/nccsv_verifier/NccsvVerifierMethods.cs
@@ -4,20 +4,18 @@ namespace nccsv_verifier
 {
     public class NccsvVerifierMethods
     {
+        // Returns true if input file is in utf-8 encoding, false if not
         public bool Utf8Checker(string filePath)
         {
             DetectionResult result = CharsetDetector.DetectFromFile(filePath);
             DetectionDetail resultDetected = result.Detected;
             string encodingName = resultDetected.EncodingName;
 
-            //return encodingName;
             if (encodingName == "utf-8")
             {
                 return true;
             }
-
             return false;
-
         }
     }
 }
--- nccsv_converter/nccsv_verifier_tests/NccsvVerifierMethods_Tests.cs
+++ nccsv_converter/nccsv_verifier_tests/NccsvVerifierMethods_Tests.cs
@@ -11,13 +11,27 @@ namespace nccsv_verifier.Tests
         {
             //Arrange
             var sut = new NccsvVerifierMethods();
-            string filePath = "C:\\SND_repos\\nccsv_netcdf\\nccsv_converter\\nccsv_verifier_tests\\testcase-utf-8-true.csv";
+            string filePath = "C:\\SND\\Project\\nccsv_converter\\nccsv_verifier_tests\\testcase-utf-8-true.csv";
 
             //Act 
             var result = sut.Utf8Checker(filePath);
 
             //Assert
             Assert.True(result);
+        }
+
+        [Fact]
+        public void Utf8Checker_ReturnsFalseIfFileIsNotUtf8()
+        {
+            //Arrange
+            var sut = new NccsvVerifierMethods();
+            string filePath = "C:\\SND\\Project\\nccsv_converter\\nccsv_verifier_tests\\testxase-utf-8-false.csv";
+
+            //Act 
+            var result = sut.Utf8Checker(filePath);
+
+            //Assert
+            Assert.False(result);
         }
     }
 }
