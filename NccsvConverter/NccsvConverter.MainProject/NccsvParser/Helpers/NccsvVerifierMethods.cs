7e3f41d460a896def34df293724608c1faada16b
idaboren <idaboren@gmail.com>
2023-08-25 12:55:25 +0200

    deleted utf-8 checker

--- NccsvConverter/NccsvConverter.MainProject/NccsvParser/Helpers/NccsvVerifierMethods.cs
+++ NccsvConverter/NccsvConverter.MainProject/NccsvParser/Helpers/NccsvVerifierMethods.cs
@@ -4,20 +4,6 @@ namespace NccsvConverter.MainProject.NccsvParser.Helpers
 {
     public class NccsvVerifierMethods
     {
-        // Returns true if input file is in utf-8 encoding, false if not
-        public bool Utf8Checker(string filePath)
-        {
-            DetectionResult result = CharsetDetector.DetectFromFile(filePath);
-            DetectionDetail resultDetected = result.Detected;
-            string encodingName = resultDetected.EncodingName;
-
-            if (encodingName == "utf-8")
-            {
-                return true;
-            }
-            return false;
-        }
-
         public bool NccsvExtensionChecker(string filePath)
         {
             if (filePath.EndsWith(".nccsv"))
--- NccsvConverter/NccsvConverter.TestProject/NccsvParser/Helpers/NccsvVerifierMethods_Tests.cs
+++ NccsvConverter/NccsvConverter.TestProject/NccsvParser/Helpers/NccsvVerifierMethods_Tests.cs
@@ -4,34 +4,6 @@ namespace NccsvConverter.TestProject.NccsvParser.Helpers
 {
     public class NccsvVerifierMethods_Tests
     {
-        [Fact]
-        public void Utf8Checker_ReturnsTrueIfFileIsUtf8()
-        {
-            //Arrange
-            var sut = new NccsvVerifierMethods();
-            string filePath = "";
-
-            //Act 
-            var result = sut.Utf8Checker(filePath);
-
-            //Assert
-            Assert.True(result);
-        }
-
-        [Fact]
-        public void Utf8Checker_ReturnsFalseIfFileIsNotUtf8()
-        {
-            //Arrange
-            var sut = new NccsvVerifierMethods();
-            string filePath = "";
-
-            //Act 
-            var result = sut.Utf8Checker(filePath);
-
-            //Assert
-            Assert.False(result);
-        }
-
         [Theory]
         [InlineData("test.nccsv")]
         [InlineData(".nccsv")]
